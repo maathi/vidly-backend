4da8ae6524e787b15ad7bdaa2026eadf2e2004fa
maathi <mahdi221b@gmail.com>
2020-11-25 14:57:38 +0100

    removd

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ var schema = buildSchema(`
 
 var app = express()
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
-// app.use(express.urlencoded())
+app.use(express.urlencoded())
 
 app.get("/", function (req, res, next) {
   res.json({ msg: "tat works fine" })
